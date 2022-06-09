3d0aa62e1750b156a1d2cb0d70eaad5b86de6bb0
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-06-09 11:39:20 +0100

    Update distance calculation

--- src/main/java/com/solirius/hosehackathon/utilities/Calculate.java
+++ src/main/java/com/solirius/hosehackathon/utilities/Calculate.java
@@ -1,6 +1,5 @@
 package com.solirius.hosehackathon.utilities;
 
-import java.awt.*;
 import java.time.Duration;
 
 public class Calculate {
@@ -18,6 +17,21 @@ public class Calculate {
 
     public static double distance(double lat1, double lon1, double lat2, double lon2) {
         //Calculate distance
-        return Point.distance(lat1, lon1, lat2, lon2);
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) *
+                Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return dist;
     }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
 }
