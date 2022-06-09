c39b85453bb89dde3d8d975349ec30bc7d742d46
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-09 15:38:45 +0100

    Update Test

--- src/test/java/com/solirius/hosehackathon/service/SimpleOfficeLocatorServiceTest.java
+++ src/test/java/com/solirius/hosehackathon/service/SimpleOfficeLocatorServiceTest.java
@@ -17,27 +17,28 @@ class SimpleOfficeLocatorServiceTest {
     private SimpleOfficeLocatorService simpleOfficeLocatorService;
     private OfficeRepository officeRepository;
 
-    double userLat;
-    double userLon;
+    private Office office1;
+    private Office office2;
+    private Office office3;
 
     @BeforeEach
     void setUp() {
-        userLat = -7.743797;
-        userLon = 54.218553;
+        List<Office> officeFindAll = new ArrayList<>();
+        office1 = new Office(-0.285419, 50.828782, "Adur Estuary", false, true, false, true, true, true, true, false);
+        officeFindAll.add(office1);
+        office2 = new Office(-7.743797,54.218553, "Aghatirourke", false, true, false, true, true, true, true, false);
+        officeFindAll.add(office2);
+        office3 = new Office(-5.116096,55.25298, "Adur Estuary", false, true, false, true, true, true, true, false);
+        officeFindAll.add(office3);
         officeRepository = mock(OfficeRepository.class);
         simpleOfficeLocatorService = new SimpleOfficeLocatorService(officeRepository);
+        when(officeRepository.findAll()).thenReturn(officeFindAll);
     }
 
     @Test
     void getOffice_Normal_ReturnOffice() {
-        List<Office> officeFindAll = new ArrayList<>();
-        Office office1 = new Office(-0.285419, 50.828782, "Adur Estuary", false, true, false, true, true, true, true, false);
-        officeFindAll.add(office1);
-        Office office2 = new Office(-7.743797,54.218553, "Aghatirourke", false, true, false, true, true, true, true, false);
-        officeFindAll.add(office2);
-        Office office3 = new Office(-5.116096,55.25298, "Adur Estuary", false, true, false, true, true, true, true, false);
-        officeFindAll.add(office3);
-        when(officeRepository.findAll()).thenReturn(officeFindAll);
+        double userLat = -7.743797;
+        double userLon = 54.218553;
         Office office = simpleOfficeLocatorService.getOffice(userLat, userLon);
         assertEquals(office2, office);
     }
