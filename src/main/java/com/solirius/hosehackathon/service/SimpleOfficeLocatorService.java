475b6ce3b34c8d39de770902d0efa8a623bf7a58
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-09 14:48:39 +0100

    Add getOffice

--- src/main/java/com/solirius/hosehackathon/service/SimpleOfficeLocatorService.java
+++ src/main/java/com/solirius/hosehackathon/service/SimpleOfficeLocatorService.java
@@ -1,13 +1,32 @@
 package com.solirius.hosehackathon.service;
 
 import com.solirius.hosehackathon.domain.Office;
+import com.solirius.hosehackathon.repository.OfficeRepository;
+import com.solirius.hosehackathon.utilities.Calculate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SimpleOfficeLocatorService implements OfficeLocatorService {
+    
+    @Autowired
+    private OfficeRepository officeRepository;
+
     @Override
     public Office getOffice(double lat, double lng) {
-        // tpdo
-        return null;
+        Office selectedOffice = null;
+        double selectedDistance = -1L;
+        List<Office> listOffice = (List<Office>) officeRepository.findAll();
+        for (Office currentOffice : listOffice) {
+            double currentDistance =
+                    Calculate.distance(lat, lng, currentOffice.getLatitude(), currentOffice.getLongitude());
+            if (selectedDistance > currentDistance || selectedOffice == null) {
+                selectedOffice = currentOffice;
+                selectedDistance = currentDistance;
+            }
+        }
+        return selectedOffice;
     }
 }
