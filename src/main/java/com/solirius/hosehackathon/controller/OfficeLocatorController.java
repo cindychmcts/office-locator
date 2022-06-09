6fa0f3b5068ebc5f7118f100db9a08a33bb80c90
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-06-09 14:52:21 +0100

    Rewrite API

--- src/main/java/com/solirius/hosehackathon/controller/OfficeLocatorController.java
+++ src/main/java/com/solirius/hosehackathon/controller/OfficeLocatorController.java
@@ -1,9 +1,7 @@
 package com.solirius.hosehackathon.controller;
 
 import com.solirius.hosehackathon.service.OfficeLocatorService;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +14,18 @@ public class OfficeLocatorController {
         this.officeLocatorService = officeLocatorService;
     }
 
-    @GetMapping({"/locate-offices/{lat}/{lng}"})
-    public String locateOffice(@PathVariable double lat, @PathVariable double lng) {
+    @GetMapping("/locateoffice")
+    public String locateOffice(@RequestParam("latitude") double lat, @RequestParam("longitude") double lng) {
         return officeLocatorService.getOffice(lat, lng).getName();
     }
+
+//    @GetMapping("/locate-office")
+//    public String locateOffice(@RequestParam("latitude") double lat, @RequestParam("longitude") double lng,
+//                               @RequestParam(name="wifi", required=false) boolean wifi,
+//                               @RequestParam(name="extendedAccess", required=false) boolean extendedAccess,
+//                               @RequestParam(name="kitchen", required=false) boolean kitchen,
+//                               @RequestParam(name="breakArea", required=false) boolean breakArea,
+//                               @RequestParam(name="petFriendly", required=false) boolean petFriendly) {
+//        return officeLocatorService.getOffice(lat, lng).getName();
+//    }
 }
