3763489f39c43ab2372fb04f2aaba0b523cd1dff
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-06-09 12:52:31 +0100

    Add office locator service

---
+++ src/main/java/com/solirius/hosehackathon/service/SimpleOfficeLocatorService.java
@@ -0,0 +1,13 @@
+package com.solirius.hosehackathon.service;
+
+import com.solirius.hosehackathon.domain.Office;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleOfficeLocatorService implements OfficeLocatorService {
+    @Override
+    public Office getOffice(double lat, double lng) {
+        // tpdo
+        return null;
+    }
+}
